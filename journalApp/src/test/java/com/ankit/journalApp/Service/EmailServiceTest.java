e1daedb06ee8e64f99e8bbf805723e8c2f6a3547
Ankit Jaiswal <jayswalankit94@gmail.com>
2026-03-25 11:19:19 +0530

    test: update email service test case

--- journalApp/src/test/java/com/ankit/journalApp/Service/EmailServiceTest.java
+++ journalApp/src/test/java/com/ankit/journalApp/Service/EmailServiceTest.java
@@ -1,4 +1,22 @@
 package com.ankit.journalApp.Service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class EmailServiceTest {
+
+
+
+    @Autowired
+    private EmailService emailService;
+
+
+    @Test
+    void testSendMail(){
+        System.out.println("USERNAME: " + System.getenv("MAIL_USERNAME"));
+        System.out.println("PASSWORD: " + System.getenv("MAIL_PASSWORD"));
+        emailService.sendEmail("jayswalankit94@gmail.com","Testing Java Mail Sender","site, works successfully");
+    }
 }
